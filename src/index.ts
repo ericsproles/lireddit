c8c14adba5d6ee6b65e62a7c55761c7c9182e891
Eric Sproles <tron04@gmail.com>
2020-12-03 14:46:49 -0800

    create first post

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,19 @@ import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 
 const main = async () => {
+  // Connect to DB
   const orm = await MikroORM.init(microConfig);
-
+  // Run Migrations
+  await orm.getMigrator().up();
+  // Run SQL
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
-console.log("hello new york");
+main().catch((err) => {
+  console.error(err);
+});
+
+console.log("hello new yorkk");
