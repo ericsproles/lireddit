cc664273faca7391568deee003cc70c3171730a9
Eric Sproles <tron04@gmail.com>
2020-12-11 17:19:56 -0800

    create query for finding a post

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   // Connect to DB
@@ -11,6 +15,17 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
   app.get("/", (_, res) => {
     res.send("hello");
   });
