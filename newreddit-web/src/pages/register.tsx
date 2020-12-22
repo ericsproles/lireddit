d6d22563b9d3111e0a91303d8891fcc132659fa3
Eric Sproles <tron04@gmail.com>
2020-12-22 14:46:35 -0800

    add gql mutation to register page

--- newreddit-web/src/pages/register.tsx
+++ newreddit-web/src/pages/register.tsx
@@ -3,16 +3,37 @@ import { Form, Formik } from "formik";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username:String!, $password:String!){
+  register(options: { username: $username, password: $password}) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [{}, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
+        onSubmit={async (values) => {
           console.log(values);
+          const response = await register(values);
+          console.log(
+            "🚀 ~ file: register.tsx ~ line 33 ~ onSubmit={ ~ response",
+            response
+          );
         }}
       >
         {({ isSubmitting }) => (
