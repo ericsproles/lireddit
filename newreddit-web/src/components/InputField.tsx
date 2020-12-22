f110c6794b2e165774ac0b113854480b54022d08
Eric Sproles <tron04@gmail.com>
2020-12-21 20:57:00 -0800

    merge web folder in

---
+++ newreddit-web/src/components/InputField.tsx
@@ -0,0 +1,28 @@
+import React, { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+
+type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size,
+  ...props
+}) => {
+  const [field, { error }] = useField(props);
+  return (
+    <FormControl isInvalid={!!error}>
+      <FormLabel htmlFor="name">{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} placeholder="name" />
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+};
