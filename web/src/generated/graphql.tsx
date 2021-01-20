7379a90056086557a0ca92752217d865bf207923
Eric Sproles <tron04@gmail.com>
2021-01-20 12:51:15 -0800

    feat: add text to posts query

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -240,7 +240,7 @@ export type PostsQuery = (
   { __typename?: 'Query' }
   & { posts: Array<(
     { __typename?: 'Post' }
-    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt'>
+    & Pick<Post, 'id' | 'title' | 'createdAt' | 'updatedAt' | 'text'>
   )> }
 );
 
@@ -353,6 +353,7 @@ export const PostsDocument = gql`
     title
     createdAt
     updatedAt
+    text
   }
 }
     `;
