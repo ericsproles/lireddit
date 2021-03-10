4105afcb3b98dc537342ea62fe3318fa39f485d2
Eric Sproles <tron04@gmail.com>
2021-03-10 12:50:10 -0800

    chore: update env types

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -1,8 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
-    POSTGRES_USERNAME: string;
-    POSTGRES_PASSWORD: string;
     ETHEREAL_USERNAME: string;
     ETHEREAL_PASWORD: string;
     REDIS_URL: string;
