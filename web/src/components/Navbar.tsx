c086cefada282e9c9791f544af208a18e37f5c6f
Eric Sproles <tron04@gmail.com>
2021-03-10 12:58:51 -0800

    fix: NavBar remove unused imports

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Link, MenuItem } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
